935c13c50723838033b0bf7023c841b657d1c473
Eduardo Vera Sousa <eduardo.sousa@dell.com>
2020-06-02 16:39:45 -0300

    Fix: python frontend when building a vector due to Boost-STL mapping

--- python/src/macro.hpp
+++ python/src/macro.hpp
@@ -1,21 +1,20 @@
 /* Copyright (C) Eduardo Vera Sousa and Leandro Augusto Frata Fernandes
- * 
+ *
  * authors    : Sousa, Eduardo V.
  *              Fernandes, Leandro A. F.
  * repository : https://github.com/Prograf-UFF/TbGAL
- * 
+ *
  * This file is part of the Tensor-based Geometric Algebra Library (TbGAL).
- * 
+ *
  * TbGAL is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * TbGAL is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
  * You should have received a copy of the GNU General Public License
  * along with TbGAL. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -238,12 +237,16 @@
     py::def("scalar", +[](std::int32_t value) { return scalar(DefaultScalarType(value)); }); \
     py::def("scalar", +[](std::int64_t value) { return scalar(DefaultScalarType(value)); }); \
     py::def("scalar", +[](DefaultScalarType value) { return scalar(value); }); \
-    py::def("vector", py::raw_function(+[](py::tuple const &args, py::dict const &) { return vector(py::stl_input_iterator<DefaultScalarType>(args), py::stl_input_iterator<DefaultScalarType>()); }))
+    py::def("vector", py::raw_function(+[](py::tuple const &args, py::dict const &) { \
+        std::vector<DefaultScalarType> dummy; \
+        dummy.assign(py::stl_input_iterator<DefaultScalarType>(args), py::stl_input_iterator<DefaultScalarType>()); \
+        return vector(dummy.begin(), dummy.end()); \
+    }));
 
 #define PY_TBGAL_EXPOSE_FUNCTION(NAME, FUNCTION) \
     py::def(NAME, &FUNCTION)
 
 #define PY_TBGAL_EXPOSE_VARIABLE(NAME, INSTANCE) \
-    py::scope().attr(NAME) = py::object(py::ptr(&INSTANCE))
+    py::scope().attr(NAME) = py::ptr(&INSTANCE)
 
 #endif // __TBGAL_PYTHON_MACRO_HPP__
--- python/src/macro_Minkowski.hpp
+++ python/src/macro_Minkowski.hpp
@@ -1,21 +1,21 @@
 /* Copyright (C) Eduardo Vera Sousa and Leandro Augusto Frata Fernandes
- * 
+ *
  * authors    : Sousa, Eduardo V.
  *              Fernandes, Leandro A. F.
  * repository : https://github.com/Prograf-UFF/TbGAL
- * 
+ *
  * This file is part of the Tensor-based Geometric Algebra Library (TbGAL).
- * 
+ *
  * TbGAL is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * TbGAL is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with TbGAL. If not, see <https://www.gnu.org/licenses/>.
  */
@@ -30,7 +30,15 @@
         .def("set_base_space_dimensions", &METRIC_SPACE_TYPE::set_base_space_dimensions)
 
 #define PY_TBGAL_EXPOSE_MINKOWSKI_UTILS() \
-    py::def("euclidean_vector", py::raw_function(+[](py::tuple const &args, py::dict const &) { return euclidean_vector(py::stl_input_iterator<tbgal::DefaultScalarType>(args), py::stl_input_iterator<tbgal::DefaultScalarType>()); })); \
-    py::def("point", py::raw_function(+[](py::tuple const &args, py::dict const &) { return point(py::stl_input_iterator<tbgal::DefaultScalarType>(args), py::stl_input_iterator<tbgal::DefaultScalarType>()); }))
+    py::def("euclidean_vector", py::raw_function(+[](py::tuple const &args, py::dict const &) { \
+        std::vector<DefaultScalarType> dummy; \
+        dummy.assign(py::stl_input_iterator<DefaultScalarType>(args), py::stl_input_iterator<DefaultScalarType>()); \
+        return vector(dummy.begin(), dummy.end()); \
+    })); \
+    py::def("point", py::raw_function(+[](py::tuple const &args, py::dict const &) { \
+        std::vector<DefaultScalarType> dummy; \
+        dummy.assign(py::stl_input_iterator<DefaultScalarType>(args), py::stl_input_iterator<DefaultScalarType>()); \
+        return vector(dummy.begin(), dummy.end()); \
+    }));
 
 #endif // __TBGAL_PYTHON_MACRO_MINKOWSKI_HPP__
